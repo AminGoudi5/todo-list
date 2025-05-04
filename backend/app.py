1e182f450d1a9b1500a182e1fa9d1792ddc5a55f
AminGoudi5 <118167890+AminGoudi5@users.noreply.github.com>
2025-05-04 10:09:32 +0330

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,16 @@ from blueprints.panel import panel
 
 app = Flask(__name__)
 app.secret_key =os.environ.get('SECRET_KEY', 'fallback-secret')
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['MYSQL_CONFIG']
+
+
+db_user = os.environ.get("MYSQLUSER")
+db_password = os.environ.get("MYSQLPASSWORD")
+db_host = os.environ.get("MYSQLHOST")
+db_port = os.environ.get("MYSQLPORT")
+db_name = os.environ.get("MYSQL_DATABASE")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
